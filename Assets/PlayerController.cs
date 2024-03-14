62144ddb4ddb365b4a53aa1bfa43509fabf736e8
Eduardo Abrantes <81658559+PatalJunior@users.noreply.github.com>
2024-03-14 15:33:40 +0000

    Menu test update

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Range(0.0f, 5.0f)]
+    public float speed = 1.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,17 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         
+    }
+
+   
+
+    // This function is called a fixed framerate
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * speed / 10;
+        float vertical = Input.GetAxis("Vertical") * speed / 10;
+
+        transform.Translate(Vector3.right * horizontal);
+        transform.Translate(Vector3.up * vertical);
     }
 }
