71d4c2c89db8900994a2a620fdeb6abd80ec4f18
oSLaYN <rodrigo.6640@gmail.com>
2024-04-04 17:37:39 +0100

    Upload Audios & Changes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,8 +5,14 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
+    public AudioClip selectAudio;
+    //AudioSource source;
     public void PlayGame()
     {
+        //source = GetComponent<AudioSource>();
+        //source.clip = selectAudio;
+        //source.Play();
+        
         SceneManager.LoadSceneAsync("Level 1");
     }
 
